4460a503831b2ea61d9689a17ba7352ea83e8a13
ukatama <dev.ukatama@gmail.com>
2016-12-03 15:26:56 +0900

    gh-pages

--- src/actions/Control.js
+++
@@ -1,4 +0,0 @@
-import { createAction } from 'redux-actions';
-
-export const TOGGLE = 'CONTROL/TOGGLE';
-export const toggle = createAction(TOGGLE);
--- src/reducers/controlIsVisible.js
+++
@@ -1,6 +0,0 @@
-import { handleActions } from 'redux-actions';
-import * as Actions from '../actions/Control';
-
-export default handleActions({
-    [Actions.TOGGLE]: state => !state,
-}, false);
